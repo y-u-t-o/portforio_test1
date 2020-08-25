92742d46f738cbe20c909cffc8e430c87737d09f
y-u-t-o <63902764+y-u-t-o@users.noreply.github.com>
2020-08-25 11:17:31 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ const nav = document.querySelector('nav');
 const border1 = document.querySelector('.border1');
 const border2 = document.querySelector('.border2');
 const border3 = document.querySelector('.border3');
-const link = document.querySelector('.link');
+const link1 = document.querySelector('.link1');
+const link2 = document.querySelector('.link2');
+const link3 = document.querySelector('.link3');
+const link4 = document.querySelector('.link4');
+const link5 = document.querySelector('.link5');
 
 btn.addEventListener('click', function () {
   $('.welcome').toggleClass('close');
@@ -20,7 +24,67 @@ btn.addEventListener('click', function () {
   border3.classList.toggle('open3');
 });
 
-link.addEventListener('click', function () {
+link1.addEventListener('click', function () {
+  $('nav').toggleClass('open-menu');
+  $(".welcome").toggleClass("close");
+  $(".about").toggleClass("close");
+  $(".service").toggleClass("close");
+  $(".skill").toggleClass("close");
+  $(".work").toggleClass("close");
+  $(".contact").toggleClass("close");
+  $(".footer").toggleClass("close");
+  $("body").toggleClass("hidden");
+  border1.classList.toggle("open1");
+  border2.classList.toggle("open2");
+  border3.classList.toggle("open3");
+});
+
+link2.addEventListener('click', function () {
+  $('nav').toggleClass('open-menu');
+  $(".welcome").toggleClass("close");
+  $(".about").toggleClass("close");
+  $(".service").toggleClass("close");
+  $(".skill").toggleClass("close");
+  $(".work").toggleClass("close");
+  $(".contact").toggleClass("close");
+  $(".footer").toggleClass("close");
+  $("body").toggleClass("hidden");
+  border1.classList.toggle("open1");
+  border2.classList.toggle("open2");
+  border3.classList.toggle("open3");
+});
+
+link3.addEventListener('click', function () {
+  $('nav').toggleClass('open-menu');
+  $(".welcome").toggleClass("close");
+  $(".about").toggleClass("close");
+  $(".service").toggleClass("close");
+  $(".skill").toggleClass("close");
+  $(".work").toggleClass("close");
+  $(".contact").toggleClass("close");
+  $(".footer").toggleClass("close");
+  $("body").toggleClass("hidden");
+  border1.classList.toggle("open1");
+  border2.classList.toggle("open2");
+  border3.classList.toggle("open3");
+});
+
+link4.addEventListener('click', function () {
+  $('nav').toggleClass('open-menu');
+  $(".welcome").toggleClass("close");
+  $(".about").toggleClass("close");
+  $(".service").toggleClass("close");
+  $(".skill").toggleClass("close");
+  $(".work").toggleClass("close");
+  $(".contact").toggleClass("close");
+  $(".footer").toggleClass("close");
+  $("body").toggleClass("hidden");
+  border1.classList.toggle("open1");
+  border2.classList.toggle("open2");
+  border3.classList.toggle("open3");
+});
+
+link5.addEventListener('click', function () {
   $('nav').toggleClass('open-menu');
   $(".welcome").toggleClass("close");
   $(".about").toggleClass("close");
